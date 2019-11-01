ca1c6f0dc8ddc598f70c8a1348dad71faaead331
Vishnu Katpure <vishnu.katpure@AD.MSYSTECHNOLOGIES.COM>
2019-11-01 10:46:17 +0530

    rename folder name and pom changes

--- src/main/java/com/example/config/SpringBootExampleApplication.java
+++ src/main/java/com/example/config/SpringBootExampleApplication.java
@@ -1,4 +1,4 @@
-package com.examle.springbootexample;
+package com.example.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -0,0 +1,21 @@
+package com.example.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.Employee;
+
+@RestController
+public class EmployeeController {
+
+	@RequestMapping("/")
+	public List<Employee> getEmployees() {
+		List<Employee> employeesList = new ArrayList<Employee>();
+		employeesList.add(new Employee(1, "lokesh", "gupta", "howtodoinjava@gmail.com"));
+		return employeesList;
+	}
+
+}
